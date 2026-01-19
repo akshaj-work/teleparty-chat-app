612cc1652255056986d181ab0ddc9ee24f0c7e26
Akshaj Jha <akshajjha@Akshajs-MacBook-Air.local>
2026-01-19 05:49:53 +0530

    Final edits

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Uncomment and set base path when deploying to GitHub Pages
   base: '/teleparty-chat-app/',
 })
